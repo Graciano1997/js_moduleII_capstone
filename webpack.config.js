1dd6966f0a804d8d002caa1c34d1c41e53e7e912
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-07 20:30:55 +0100

    setup webpack project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable  */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  mode:'development',
+  devtool: 'inline-source-map',
+  devServer:{
+    static:'./dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(mp3|ogg|wav)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'audio/',
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
