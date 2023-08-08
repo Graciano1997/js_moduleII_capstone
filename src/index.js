107266b522551e69df08e2bfa733b7d1b75610a4
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-08-08 23:41:39 +0100

    add display moovie list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './css/index.css';
+import { displayMoov, full_moov_array } from './modules/apiController.js';
+
+for (let index = 1; index < 7; index++) {
+    displayMoov(index);
+}
+
+console.log(full_moov_array);
