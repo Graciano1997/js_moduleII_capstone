ce881db0d176ebe184cf3615831eac7583617057
Bett Enock <bettenock968@gmail.com>
2023-08-11 23:44:50 -0700

    fetch and display comments from api.

--- src/modules/commentController.js
+++ src/modules/commentController.js
@@ -1,4 +1,33 @@
 import * as Variable from './globalVar.js';
+import counterItem from './itemCounter.js';
+
+const createCommenter = (data, type) => {
+  const el = document.createElement(type);
+  el.innerHTML = `<b class='date'>${data.creation_date}</b> ${data.username} : <span class ="comment">’’ ${data.comment} ’’</span>`;
+  return el;
+};
+
+const noCommentFunction = () => {
+  const el = document.createElement('p');
+  el.classList.add('noComment');
+  el.textContent = 'No comments !!';
+  return el;
+};
+const commentsReader = async (id) => {
+  await fetch(`${Variable.involvementApiUrl}comments?item_id=${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      while (document.querySelector('.user-comments').firstChild) {
+        document.querySelector('.user-comments').firstChild.remove();
+      }
+      if (json.length >= 0) {
+        json.forEach((comment) => {
+          document.querySelector('.user-comments').append(createCommenter(comment, 'p'));
+        });
+        document.querySelector('#commentsCounter').innerHTML = counterItem('#comments-item');
+      } else { document.querySelector('.user-comments').append(noCommentFunction()); }
+    });
+};
 
 const commentMoov = async (commentObject) => {
   await fetch(`${Variable.involvementApiUrl}comments`, {
@@ -10,6 +39,7 @@ const commentMoov = async (commentObject) => {
     }),
     headers: { 'Content-type': 'application/json; charset=UTF-8' },
   });
+  commentsReader(commentObject.item_id);
 };
 
-export default { commentMoov };
+export { commentMoov, commentsReader };
